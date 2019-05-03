29b4911c5a17ef0cc3f2671492c71b37ea3cfe43
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-05-03 15:15:30 +0530

    Create powerr.py

---
+++ powerr.py
@@ -0,0 +1,10 @@
+def is-power-of-two(n):
+retrun true if n is a power of two
+if n<=0:
+retrun false
+else
+return&(n-1)==0
+if power of two(n)
+print("power of two")
+else
+print("not power of two")
